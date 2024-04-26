a2b0ea384606a5772ac817aed685945da89625a0
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-26 23:09:52 +0300

    add week14 code

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+
+Dog neighboursDog = new Dog("Good Girl");
+
+
+Console.WriteLine($"My dogs name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dog name is {neighboursDog.Name}.");
+
+myDog.Rename("Bad Boy");
+
+
+while (myDog.LevelOfHapiness != 5)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+class Dog
+{
+    private string _name;
+    private int _levelOfHapiness;
+
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to: {newName}");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof!");
+        _levelOfHapiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
